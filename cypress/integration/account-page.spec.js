0b249f48f23ef443a3e8a9d32355919a24bc566b
Walter Palombini <walterpalombini@gmail.com>
2021-08-14 23:14:44 +1000

    Prevents unauthorized access to account page test

--- cypress/integration/account-page.spec.js
+++ cypress/integration/account-page.spec.js
@@ -23,4 +23,11 @@ describe('Account Page', () => {
 
     cy.get(byDataTest('title-account-page')).should('have.text', 'Account Details Page');
   });
+
+  it('prevents unauthorized access to account page', () => {
+    cy.visit('/private/account');
+
+    cy.url().should('equal', 'http://localhost:3000/login');
+    cy.get(byDataTest('title-login-page')).should('have.text', 'Login Page');
+  });
 });
